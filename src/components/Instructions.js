8e07eca5f9933a300f9e2e4d777db84e056a00ab
lovodkin93 <lovodkin93@gmail.com>
2022-02-26 19:40:17 +0200

    refinements to the opening page

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom'
 import BackButton from './BackButton'
 
 const Instructions = () => {
--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,7 +1,5 @@
 import { Link } from 'react-router-dom';
 import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import Button from '@material-ui/core/Button';
 
 
 
@@ -13,31 +11,42 @@ const StartPage = () => {
             Highlighting Extraction UI
           </h2>
       </header>
-
-      <section>
-        <p className='StartPageText'>
-          Welcome to the Highlighting Extraction UI.
-          <br></br>
-          Before starting annotating, please read the Instructions first and practice using the Guided Annotation option.
-        </p>
-      </section>
       
-      <section>
-        <Stack spacing={3} justifyContent="space-evenly" alignItems="center">
+      <div className='startPageBody'>
 
-          <Button component={Link} to="/instructions" variant="contained" color="primary" size="large">
+          <p className='StartPageText'>
+            Welcome to the Highlighting Extraction UI.
+            <br></br>
+            Before starting annotating, please read the Instructions first and practice using the Guided Annotation option.
+          </p>
+      
+          <Link 
+              role="button"
+              className='StartPageButton'
+              id="instructionStartButton"
+              to="/instructions"
+          >
             Instructions
-          </Button>
-          
-          <Button style={{maxWidth: '35%'}} component={Link} to="/guidedAnnotation" variant="contained" color="primary" size="large">
-            Guided Annotation
-          </Button>
+          </Link>
 
-          <Button style={{maxWidth: '35%'}} component={Link} to="/annotation" variant="contained" color="primary" size="large">
+          <Link 
+              role="button"
+              className='StartPageButton'
+              id="guidedAnnotationStartButton"
+              to="/guidedAnnotation"
+          >
+            Guided Annotation
+          </Link>
+
+          <Link 
+              role="button"
+              className='StartPageButton'
+              id="annotationStartButton"
+              to="/annotation"
+          >
             Annotation
-          </Button>
-        </Stack>
-      </section>
+          </Link>
+      </div>
     </>
   )
 }
