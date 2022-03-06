7e06be4fa66c950fc4bc781b227cec6feef0de6b
lovodkin93 <lovodkin93@gmail.com>
2022-03-06 13:06:43 +0200

    cleaned functions inside the Annotation component that were moved to a different js file

--- src/components/Annotation.js
+++ src/components/Annotation.js
@@ -12,10 +12,12 @@ const Annotation = ({task_id, doc_json, summary_json, lemma_match_mtx, toggleSum
   // console.log(summary_json)
   // console.log(lemma_match_mtx)
   const [showAddTask, setShowAddTask] = useState(false)
+  
   const [DocMouseclickStartID, SetDocMouseDownStartID] = useState("-1");
   const [DocMouseclicked, SetDocMouseclicked] = useState(false);
   const [SummaryMouseclickStartID, SetSummaryMouseDownStartID] = useState("-1");
   const [SummaryMouseclicked, SetSummaryMouseclicked] = useState(false);
+  const [StateMachineState, SetStateMachineState] = useState("Start");
 
 
   const DocMouseClickHandlerWrapper = (tkn_id) => {
@@ -26,35 +28,7 @@ const Annotation = ({task_id, doc_json, summary_json, lemma_match_mtx, toggleSum
     SummaryMouseClickHandler({tkn_id, toggleSummaryHighlight, SummaryMouseclickStartID, SummaryMouseclicked, SetSummaryMouseDownStartID, SetSummaryMouseclicked});
   }
 
-  // const DocMouseClickHandler = (tkn_id) => {
-  //   const update_tkn = DocMouseclicked ? "-1" : tkn_id;
-  //   if (DocMouseclicked){
-  //     const min_ID =  (DocMouseclickStartID > tkn_id) ? tkn_id : DocMouseclickStartID;
-  //     const max_ID =  (DocMouseclickStartID > tkn_id) ? DocMouseclickStartID : tkn_id;
-  //     let chosen_IDs = [];
-  //     for(let i=min_ID; i<=max_ID; i++){
-  //       chosen_IDs.push(i);
-  //     }
-  //     toggleDocHighlight(chosen_IDs);     
-  //   }
-  //   SetDocMouseDownStartID(update_tkn);
-  //   SetDocMouseclicked(!DocMouseclicked);
-  // }
 
-  // const SummaryMouseClickHandler = (tkn_id) => {
-  //   const update_tkn = SummaryMouseclicked ? "-1" : tkn_id;
-  //   if (SummaryMouseclicked){
-  //     const min_ID =  (SummaryMouseclickStartID > tkn_id) ? tkn_id : SummaryMouseclickStartID;
-  //     const max_ID =  (SummaryMouseclickStartID > tkn_id) ? SummaryMouseclickStartID : tkn_id;
-  //     let chosen_IDs = [];
-  //     for(let i=min_ID; i<=max_ID; i++){
-  //       chosen_IDs.push(i);
-  //     }
-  //     toggleSummaryHighlight(chosen_IDs);     
-  //   }
-  //   SetSummaryMouseDownStartID(update_tkn);
-  //   SetSummaryMouseclicked(!SummaryMouseclicked);
-  // }
 
   return (
       <>
